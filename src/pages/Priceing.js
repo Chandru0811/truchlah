658a100ca19aa82905b50d961fc91f7c644d4248
Sumaiya-904 <sumaiyabee211@gmail.com>
2024-05-27 17:44:13 +0530

    pricing card completed

--- src/pages/Priceing.js
+++ src/pages/Priceing.js
@@ -1,51 +1,101 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import Carousel from "react-multi-carousel";
 import "react-multi-carousel/lib/styles.css";
-import Vehicle1 from "../asset/Vechicle1.png";
-import Vehicle2 from "../asset/Vechicle2.png";
-import Vehicle3 from "../asset/Vechicle3.png";
-import Vehicle4 from "../asset/Vechicle4.png";
-
+import { FaWeightHanging, FaCheckCircle } from "react-icons/fa";
+import { Modal, Button } from "react-bootstrap";
+import VAN1 from "../../src/asset/1.7M_VAN.png";
+import VAN2 from "../../src/asset/2.4M_VAN.png";
+import Lorry10 from "../../src/asset/10FT_LORRY.png";
+import Lorry14 from "../../src/asset/14FT_LORRY.png";
+import Lorry24 from "../../src/asset/24FT_LORRY.png";
 
 
 const Pricing = () => {
-  const productList = [
+  const [selectedItemId, setSelectedItemId] = useState(null);
+  const [modalShow, setModalShow] = useState(false);
+  const [currentItem, setCurrentItem] = useState([]);
+  const handleShowModal = (item, index) => {
+    setCurrentItem(item);
+    setSelectedItemId(index);
+    setModalShow(true);
+  };
+
+  const handleCloseModal = () => {
+    setModalShow(false);
+    setCurrentItem();
+  };
+  function handleCardClick(index) {
+    setSelectedItemId(index);
+  }
+  const carouselItems = [
     {
-      id: 1,
-      img: Vehicle1,
-      name: "5ft Van",
-      weight: `500 kg`,
-      base_fare: `42+0.75/Km`,
+      image: VAN1,
+      text1: "Base fare $20",
+      price: "$200",
+      name: "1.7M_VAN",
+      weight: "200kg",
+      text3: "Load body size",
+      size: " 50x50 ",
+      description:
+        "Base fare includes 1.0 km distance and 25 minutes of time. Pricing may vary based on your locality. Road tax, parking etc as applicable over and above ride fare.",
     },
     {
-      id: 2,
-      img: Vehicle2,
-      name: "10ft Lorry",
-      weight: `700 kg`,
-      base_fare: `50+0.75/Km`,
+      image: VAN2,
+      text1: "Base fare $30",
+      price: "$400",
+      name: "2.4M_VAN",
+      weight: "400kg",
+      text3: "Load body size",
+      size: " 50x50 ",
+      description:
+        "Base fare includes 1.0 km distance and 25 minutes of time. Pricing may vary based on your locality. Road tax, parking etc as applicable over and above ride fare.",
     },
     {
-      id: 3,
-      img: Vehicle3,
-      name: "14ft Lorry",
-      weight: `900 kg`,
-      base_fare: `52+0.75/Km`,
+      image: Lorry10,
+      text1: "Base fare $40",
+      price: "$600",
+      weight: "600kg",
+      name: "10FT_LORRY",
+      text3: "Load body size  ",
+      size: " 50x50 ",
+      description:
+        "Base fare includes 1.0 km distance and 25 minutes of time. Pricing may vary based on your locality. Road tax, parking etc as applicable over and above ride fare.",
     },
     {
-      id: 4,
-      img: Vehicle4,
-      name: "18ft Lorry",
-      weight: `1000 kg`,
-      base_fare: `55+0.75/Km`,
+      image: Lorry14,
+      text1: "Base fare $25",
+      price: "$450",
+      name: "14FT_LORRY",
+      weight: "800kg",
+      text3: "Load body size  ",
+      size: " 50x50 ",
+      description:
+        "Base fare includes 1.0 km distance and 25 minutes of time. Pricing may vary based on your locality. Road tax, parking etc as applicable over and above ride fare.",
     },
     {
-      id: 5,
-      img: Vehicle2,
-      name: "20ft Lorry",
-      weight: `1500 kg`,
-      base_fare: `57+0.75/Km`,
+      image: Lorry24,
+      text1: "Base fare $250",
+      price: "$550",
+      name: "24FT_LORRY",
+      weight: "100kg",
+      text3: "Load body size ",
+      size: " 50x50 ",
+      description:
+        "Base fare includes 1.0 km distance and 25 minutes of time. Pricing may vary based on your locality. Road tax, parking etc as applicable over and above ride fare.",
     },
+    {
+      image: VAN1,
+      text1: "Base fare $250",
+      price: "$1000",
+      name: "1.7M_VAN",
+      weight: "200kg",
+      text3: "Load body size  ",
+      size: " 500x500 ",
+      description:
+        "Base fare includes 1.0 km distance and 25 minutes of time. Pricing may vary based on your locality. Road tax, parking etc as applicable over and above ride fare.",
+    },
+    // Add more card data as needed
   ];
 
   const productResponsive = {
@@ -82,50 +132,140 @@ const Pricing = () => {
   };
 
   return (
-    <div className="pricing-container">
+    <div className="pricing-container pt-4">
       <Carousel
-        swipeable
-        draggable
-        showDots={false}
-        responsive={productResponsive}
-        ssr
-        infinite
+        additionalTransfrom={0}
+        arrows
+        autoPlaySpeed={3000}
         autoPlay={false}
-        autoPlaySpeed={2000}
-        keyBoardControl
-        customTransition="all .5"
-        transitionDuration={500}
-        containerClass="carousel-container"
-        removeArrowOnDeviceType={["mobile"]}
+        centerMode={false}
+        className="carousel-container"
+        containerClass="container-with-dots"
         dotListClass=""
+        draggable
+        focusOnSelect={false}
+        infinite
+        itemClass=""
+        keyBoardControl
+        minimumTouchDrag={80}
+        renderButtonGroupOutside={false}
+        renderDotsOutside={false}
+        responsive={{
+          desktop: {
+            breakpoint: { max: 3000, min: 1024 },
+            items: 4,
+            slidesToSlide: 1, // Number of slides to slide when using arrows
+          },
+          tablet: {
+            breakpoint: { max: 1024, min: 464 },
+            items: 2,
+            slidesToSlide: 1,
+          },
+          mobile: {
+            breakpoint: { max: 464, min: 0 },
+            items: 1,
+            slidesToSlide: 1,
+          },
+        }}
+        showDots={false}
+        sliderClass=""
+        slidesToSlide={1}
+        swipeable
       >
-        {productList.map((product) => (
-          <div key={product.id} className="product-item card h-100 mx-1">
-            <div
-              className="d-flex justify-content-center align-items-center"
-              style={{ height: "200px" }}
-            >
-              <div
-                className="d-flex align-items-center justify-content-center image-container"
-                style={{ maxHeight: "100%", maxWidth: "93%" }}
-              >
-                <img
-                  src={product.img}
-                  alt={product.name}
-                  className="img-fluid"
-                  style={{ maxHeight: "90%", maxWidth: "83%" }}
+        {carouselItems.map((item, index) => (
+          <label
+            className="container"
+            key={index}
+            id={`Vehicle-${index}`}
+            onClick={() => handleCardClick(index)}
+            style={{
+              cursor: 'pointer',
+              display: 'block',
+              borderRadius: '8px'
+            }}
+          >
+            <div className="card d-flex flex-column"
+             style={{ borderRadius: "8px", border: selectedItemId === index ? '3px solid #2A0944' : '1px solid #787A91' }}>
+              {selectedItemId === index && (
+                <FaCheckCircle
+                  style={{
+                    position: 'absolute',
+                    top: '0px',
+                    right: '0px',
+                    color: '#5BBCFF',
+                    fontSize: '24px',
+                    zIndex : 5,
+                  }}
                 />
+              )}
+              <img src={item.image}  alt="Card" />
+              <div className="card-body">
+                <div className="card-text">
+                  <div className="d-flex justify-content-center">
+                    <span className="p-1" style={{ background: "#D2E0FB", borderRadius: "5px" }}>
+                      <FaWeightHanging />  {item.weight}
+                    </span>
+                  </div>
+                  <div className="d-flex justify-content-center">
+                    <span style={{ fontSize: "20px" }}><b>{item.name}</b></span>
+                  </div>
+                  <div className="d-flex justify-content-center">
+                    <span>Starting from <b style={{ fontSize: "18px" }}>{item.price}</b></span>
+                  </div>
+                  <div className="d-flex justify-content-center pt-2">
+                    <span>
+                      <a href="#" onClick={(e) => { e.preventDefault(); handleShowModal(item); }}
+                        class="link-primary link-offset-2 link-underline-opacity-25 link-underline-opacity-100-hover">Know More</a>
+                    </span>
+                  </div>
+
+                  <center>
+                    <div className="form-check">
+                      <input
+                        className="form-check-input border-info"
+                        type="radio"
+                        name="selectedItem"
+                        id={`flexRadioDefault${index}`}
+                        checked={selectedItemId === index}
+                        readOnly
+                      />
+                    </div>
+                  </center>
+                </div>
               </div>
             </div>
-
-            <div className="card-body pt-0">
-              <h5 className="price-title">{product.name}</h5>
-              <p className="price-text">Base Fare: ${product.base_fare}</p>
-              <p className="price-text">Weight Limit: {product.weight}</p>
-              <button className="btn mt-2">More Details</button>
-            </div>
-          </div>
+          </label>
         ))}
+        {currentItem && (
+          <Modal show={modalShow} onHide={handleCloseModal} backdropClassName="custom-backdrop">
+            <Modal.Header closeButton>
+              <Modal.Title>{currentItem.name}</Modal.Title>
+            </Modal.Header>
+            <Modal.Body>
+              <img src={currentItem.image} style={{ width: '70%' }} />
+              <div className="d-flex justify-content-center pt-3">
+                <span className="p-1" style={{ background: "#D2E0FB", borderRadius: "5px" }}>
+                  <FaWeightHanging />  {currentItem.weight}
+                </span>
+              </div>
+              <div className="d-flex justify-content-center pt-2">
+                <span style={{ fontSize: "20px" }}><b>{currentItem.name}</b></span>
+              </div>
+              <div className="d-flex justify-content-center">
+                <span>Starting from <b style={{ fontSize: "18px" }}>{currentItem.price}</b></span>
+              </div>
+              <hr />
+              <p>Base fair price : <b>{currentItem.text1}</b></p>
+              <p>{currentItem.text3}: <b>{currentItem.size}</b></p>
+              <p><b>Description:</b> {currentItem.description}</p>
+            </Modal.Body>
+            {/* <Modal.Footer>
+                <Button className="btn btn-primary btn-sm" onClick={handleCloseModal}>
+                  Close
+                </Button>
+              </Modal.Footer> */}
+          </Modal>
+        )}
       </Carousel>
       <div className="card pt-5 pb-3 " style={{ backgroundColor: "#EEF7FF" }}>
         <h3 className="">
