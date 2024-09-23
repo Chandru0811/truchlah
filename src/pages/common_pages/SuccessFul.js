5629b6227d57a6d9371e9eedddc1eb7f5b658e79
prem-pk24 <premvp24@gmail.com>
2024-09-24 01:41:37 +0530

    bug fixed

--- src/pages/common_pages/SuccessFul.js
+++ src/pages/common_pages/SuccessFul.js
@@ -1,16 +1,22 @@
 import React, { useEffect } from "react";
 import "../../styles/custom.css";
 import { Link, useSearchParams } from "react-router-dom";
-import success from "../../asset/tick.gif";
+import success from "../../asset/tick-unscreen.gif";
 import toast from "react-hot-toast";
 
 function Successfull() {
   const [searchParams] = useSearchParams();
   const type = searchParams.get("type");
   const bookingId = searchParams.get("bookingId");
-  const result = searchParams.get("result");
-  const refNo = searchParams.get("refNo");
 
+  const bookingIdString = window.location.href.split("bookingId=")[1];
+  const secondQueryPart = bookingIdString?.split("?")[1]; 
+  const secondParams = new URLSearchParams(secondQueryPart);
+
+  const result = secondParams.get("result");
+  const refNo = secondParams.get("refNo");
+
+console.log("result",secondParams)
   useEffect(() => {
     if (result === "success") {
       toast.success("Payment successful!");
@@ -18,48 +24,51 @@ function Successfull() {
   }, []);
 
   return (
-    <section className="Success">
-      <center>
-        <div className="container py-5">
-          <div className="row">
-            <div className="col-lg-3 col-md-3 col-12"></div>
-            <div className="col-lg-6 col-md-6 col-12">
-              <div className="card">
-                <center>
-                  <div className="d-flex justify-content-center align-item-center mt-2">
-                    <img
-                      src={success}
-                      style={{ border: "none", width: "200px" }}
-                      alt="Success"
-                    />
-                  </div>
-                </center>
-                <span className="fw-bolder">Booking Id: {bookingId}</span>
-                <h5 className="mt-4" style={{ color: "#1bc24a" }}>
-                  Order {result === "success" ? "Successful" : "Failed"}
-                </h5>
-                <p className="mt-2">Thank you so much for your order</p>
-                <p>Reference No: {refNo}</p>
-                <center>
-                  <Link to={`/rides?type=${type}`}>
-                    <button
-                      id="button"
-                      type="button"
-                      className="btn btn-success my-3"
-                      style={{ width: "40%" }}
-                    >
-                      Check Status
-                    </button>
-                  </Link>
-                </center>
-              </div>
+    <section className="Success d-flex justify-content-center align-items-center " style={{minHeight:"80vh"}}>
+    <div className="container">
+      <div className="row justify-content-center">
+        <div className="col-lg-4 col-md-6 col-12">
+          <div className="card p-4 text-center cardStyle position-relative" >
+            <div className="d-flex justify-content-center align-items-center mb-3 icon-container" >
+              <img src={success} className="img-fluid " alt="Success" style={{maxWidth:"250%"}}/>
             </div>
-            <div className="col-lg-3 col-md-3 col-12"></div>
+            <h4 className="titleStyle mt-5">Congratulations</h4>
+            <span className="smallTextStyle">Booking Id: {bookingId.split("?")[0]}</span>
+            <h5 className="mt-4" style={{color:"#1bc24a"}}>
+              Order {result === "success" ? "Successful" : "Failed"}
+            </h5>
+            <p className="mt-2">Thank you so much for your order</p>
+            <p>Reference No: {refNo}</p>
+            <center>
+              <Link to={`/rides?type=${type}`}>
+                <button
+                  id="button"
+                  type="button"
+                  className="btn btn-success my-3 successBtn"
+                >
+                  Done
+                </button>
+              </Link>
+            </center>
           </div>
         </div>
-      </center>
-    </section>
-  );
+      </div>
+    </div>
+  </section>
+);
 }
+
+// Styles
+
+const iconContainerStyle = {
+width: "70px",
+height: "70px",
+backgroundColor: "#1bc24a",
+borderRadius: "50%",
+display: "flex",
+justifyContent: "center",
+alignItems: "center",
+marginBottom: "1rem",
+};
 
 export default Successfull;
